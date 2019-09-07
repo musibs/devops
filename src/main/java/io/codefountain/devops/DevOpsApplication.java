dd0c220e7a640d8f46ff66f93665e2afdc12c711
Somnath Musib <musib.somnath@gmail.com>
2019-09-07 11:17:23 +1000

    Set up the base project and configure SCM & distribution management

---
+++ src/main/java/io/codefountain/devops/DevOpsApplication.java
@@ -0,0 +1,13 @@
+package io.codefountain.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+	}
+
+}
